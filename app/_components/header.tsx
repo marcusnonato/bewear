7a46898c0ee208118412abfc46ee1a8de73e46d5
Marcus Nonato <marcusononato@gmail.com>
2025-11-09 01:48:50 -0400

    fix: update login link to use correct authentication route

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -71,7 +71,7 @@ export const Header = () => {
                 <div className="flex items-center justify-between">
                   <h2 className="font-semibold">Olá. Faça seu login!</h2>
                   <Button size="icon" asChild variant="outline">
-                    <Link href="/authentication">
+                    <Link href="/auth">
                       <LogInIcon />
                     </Link>
                   </Button>
