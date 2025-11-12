26a6980266e6c306009fc8c26efd439b1dc06bc5
Marcus Nonato <marcusononato@gmail.com>
2025-11-12 16:32:10 -0400

    feat: enhance Header component with user avatar and login functionality

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { LogInIcon, LogOutIcon, MenuIcon } from "lucide-react";
+import {
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  ShoppingBasketIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -35,11 +41,72 @@ export const Header = ({ logoOnly = false }: HeaderProps) => {
   }
 
   return (
-    <header className="flex items-center justify-between p-5">
-      <Link href="/">
+    <header className="relative flex items-center justify-between p-5 md:px-11">
+      <div className="hidden md:block">
+        {session?.user ? (
+          <>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-3">
+                <Avatar>
+                  <AvatarImage
+                    src={session?.user?.image as string | undefined}
+                  />
+                  <AvatarFallback>
+                    {session?.user?.name?.split(" ")?.[0]?.[0]}
+                    {session?.user?.name?.split(" ")?.[1]?.[0]}
+                  </AvatarFallback>
+                </Avatar>
+
+                <div>
+                  <h3 className="font-semibold">
+                    Olá, {session?.user?.name.split(" ")?.[0]}!
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="flex items-center justify-between gap-2">
+            <h2 className="font-semibold">Olá. Faça seu login!</h2>
+            <Button size="icon" asChild variant="outline">
+              <Link href="/auth">
+                <LogInIcon />
+              </Link>
+            </Button>
+          </div>
+        )}
+      </div>
+      <Link className="md:hidden" href="/">
         <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
       </Link>
-      <div className="flex items-center gap-3">
+      <Link
+        className="absolute left-1/2 hidden -translate-x-1/2 lg:block"
+        href="/"
+      >
+        <Image src="/logo.svg" alt="BEWEAR" width={129} height={34} />
+      </Link>
+      <Link
+        className="absolute left-1/2 hidden -translate-x-1/2 md:block lg:hidden"
+        href="/"
+      >
+        <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
+      </Link>
+      <div className="hidden md:flex">
+        <Button variant="outline" className="cursor-pointer" size="icon">
+          <ShoppingCartIcon />
+        </Button>
+        {session?.user.id && (
+          <Button
+            variant="outline"
+            className="ml-2 cursor-pointer"
+            size={"icon"}
+            onClick={() => authClient.signOut()}
+          >
+            <LogOutIcon />
+          </Button>
+        )}
+      </div>
+      <div className="flex items-center gap-3 md:hidden">
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon">
